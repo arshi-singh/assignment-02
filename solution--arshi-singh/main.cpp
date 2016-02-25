c1609eef3d0de237448614ae48751c52a607d138
arshi-singh <arshi95@gmail.com>
2016-02-25 14:47:53 -0800

    finished assignment #2

---
+++ solution--arshi-singh/main.cpp
@@ -0,0 +1,6 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Arshi Singh <arshi95@gmail.com>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+I completed Assignment #2 with Steven Kha, Alex Chau, and Ryan Allen.
